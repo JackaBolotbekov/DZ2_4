b30891dc728a6e8aa1cca3491517d3057890d355
KamchiDjunusov <dimooooon70@gmail.com>
2022-06-16 17:51:01 +0200

    listC: A1,B5,А2,Б4,А3,Б3,A4,B2,A5,B1

--- src/Main.java
+++ src/Main.java
@@ -20,8 +20,18 @@ public class Main {
             System.out.println(listB);
             ArrayList<String> listC = new ArrayList<String>();
             for (int f = 0; f < listB.size(); f++) {
-                listC.add( listA.get(f) );
-                listC.add( listB.get(f) );
+//                A1,B5,А2,Б4,А3,Б3,A4,B2,A5,B1
+                listC.add(listA.get(0));
+                listC.add(listB.get(4));
+                listC.add(listA.get(1));
+                listC.add(listB.get(3));
+                listC.add(listA.get(2));
+                listC.add(listB.get(2));
+                listC.add(listA.get(3));
+                listC.add(listB.get(1));
+                listC.add(listA.get(4));
+                listC.add(listB.get(0));
+
             }
             System.out.println(listC);
             for (int i = 0; i < 10; i++) {
